4f2cfecec6e38d8a23f3259811ed23880ccf5109
Sidoine De Wispelaere <sidoine@sidoine.net>
2016-05-25 23:05:58 +0200

    Error in build.ps1

--- build.ps1
+++ build.ps1
@@ -2,9 +2,9 @@ param([String]$key)
 
 $span = New-TimeSpan -Start 2010/1/1 -End (Get-Date)
 $version = "beta-" + $span.Days.ToString() + ($span.Hours * 60 + $span.Minutes).ToString("0000")
-& dotnet restore --version-suffix $version
+& dotnet restore
 cd .\src\Folke.Logger.Elm
-& dotnet pack
+& dotnet pack --version-suffix $version
 $file = Get-Item "bin\Debug\*-$version.nupkg"
 nuget push $file.FullName $key
 cd ..\..
